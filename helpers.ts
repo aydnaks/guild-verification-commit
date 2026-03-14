b601983bce0cef5a088e3218265800628e056a9a
aydnaks <aydnaks@outlook.com>
2026-03-14 21:48:11 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T18:47:17.926Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-14T18:48:07.922Z
+Commit: Patch API endpoint error handling
