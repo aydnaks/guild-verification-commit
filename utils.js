d1e52cd05ef08f8609b9203887ef8471dc7e32fe
aydnaks <aydnaks@outlook.com>
2026-03-14 21:48:14 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T18:47:01.377Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-14T18:48:11.275Z
+Commit: Resolve null pointer exception in user service
