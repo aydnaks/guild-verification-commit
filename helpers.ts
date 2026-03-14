f5bc1fbcbb8699d7d3ad9a9744e38e2f446e08bb
aydnaks <aydnaks@outlook.com>
2026-03-14 23:10:44 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:10:43.407Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-14T20:10:44.340Z
+Commit: Fix race condition in async operations
