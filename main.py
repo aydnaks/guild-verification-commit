6cb841cf6b7438a0fa866161db36051ea6ba542a
aydnaks <aydnaks@outlook.com>
2026-03-14 23:53:05 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:52:55.489Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-14T20:53:05.584Z
+Commit: Fix CSS styling bug on mobile devices
