49fbe08eca3d9d7eb6a90ae4f6e1a4ae0551da05
aydnaks <aydnaks@outlook.com>
2026-03-14 23:10:29 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T18:48:13.278Z
-Commit: Add multi-language support
+Timestamp: 2026-03-14T20:10:28.826Z
+Commit: Resolve null pointer exception in user service
