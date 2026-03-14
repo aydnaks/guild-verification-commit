d4c20fc5b189b6a97fa087f54c3f8af04eda3498
aydnaks <aydnaks@outlook.com>
2026-03-14 23:10:43 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:10:28.826Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T20:10:43.407Z
+Commit: Patch API endpoint error handling
