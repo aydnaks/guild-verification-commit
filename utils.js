4ec3d0cb3f8cd25a36d2d4b6275cfe5c60ae9cdd
aydnaks <aydnaks@outlook.com>
2026-03-14 21:47:02 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T18:46:44.061Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-14T18:46:59.541Z
+Commit: Fix race condition in async operations
