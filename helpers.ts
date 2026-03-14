d14841cdb9e23c333a4ab3398781c917206f997c
aydnaks <aydnaks@outlook.com>
2026-03-14 23:53:46 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:53:32.523Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-14T20:53:45.917Z
+Commit: Resolve null pointer exception in user service
